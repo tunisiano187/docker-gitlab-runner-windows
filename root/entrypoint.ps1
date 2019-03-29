72591bfa6b2de205ec6b803d657a9cc71d261475
tunisiano187 <tunisiano187@users.noreply.github.com>
2019-03-29 11:59:20 +0100

    Restart

--- root/entrypoint.ps1
+++ root/entrypoint.ps1
@@ -1,8 +1,8 @@
-# $installPath = 'C:\Gitlab-Runner'
-# $exeName = 'gitlab-runner.exe'
-# $serviceName = 'gitlab-runner'
+$installPath = 'C:\Gitlab-Runner'
+$exeName = 'gitlab-runner.exe'
+$serviceName = 'gitlab-runner'
 
-# $exePath = Join-Path $installPath $exeName
+$exePath = Join-Path $installPath $exeName
 
 # # stop service if it is already started
 # Stop-Service -Name $serviceName -ErrorAction SilentlyContinue
@@ -11,20 +11,19 @@
 # Get-ChildItem 'C:\startup' | Where-Object { $_.Name -match '\d+.*\.ps1' } | ForEach-Object { & $_.FullName }
 
 # # install service if not already installed
-# If (!(Get-Service $serviceName -ErrorAction SilentlyContinue))
-# {
-#     Write-Output "installing $serviceName service..."
-#     & $exePath install
+If (!(Get-Service $serviceName -ErrorAction SilentlyContinue))
+{
+     Write-Output "installing $serviceName service..."
+     & $exePath install
 
-#     Write-Output "registering $serviceName agent..."
-#     & $exePath register --non-interactive
-# }
+     Write-Output "registering $serviceName agent..."
+     & $exePath register --non-interactive
+}
 
-# Write-Output "Starting up $serviceName service..."
-# Start-Service -Name $serviceName
+Write-Output "Starting up $serviceName service..."
+Start-Service -Name $serviceName
 
-# Write-Output "Setting up watcher for $serviceName service..."
+Write-Output "Setting up watcher for $serviceName service..."
 # & .\Wait-Service.ps1 -ServiceName $serviceName
-
 
 & .\Wait-Service.ps1 -ServiceName EventLog
